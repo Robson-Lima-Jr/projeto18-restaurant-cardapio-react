7c975bc7c806a1161e2757c03ba908c542449590
Robson-Jr <robsonjuniorsn@gmail.com>
2024-10-08 14:55:06 -0300

    topo concluido

--- src/app/components/Topo/index.jsx
+++ src/app/components/Topo/index.jsx
@@ -1,13 +1,11 @@
 import styles from "./Topo.module.css";
 
 export default function Topo() {
-    return(
+    return (
         <header className={styles.container_topo}>
-            <div className={styles.conteudo_topo}>
-                <h1>Restaurant</h1>
+            <h1>Restaurant</h1>
 
-                <p>De pratos clássicos a criações surpreendentes, nosso cardápio é um requinte de sabores refinados.</p>
-            </div>
+            <p>De pratos clássicos a criações surpreendentes, nosso cardápio é um requinte de sabores refinados.</p>
         </header>
     );
 }
