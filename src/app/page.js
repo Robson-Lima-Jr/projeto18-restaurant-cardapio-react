5e17f717dbfc8e0139b81287253c253e72f7d210
Robson-Jr <robsonjuniorsn@gmail.com>
2024-10-08 16:34:36 -0300

    area dos botoes criada, a parte basica sem configurar

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,16 @@ import styles from "./page.module.css";
 
 // Importação Componentes
 import Topo from "./components/Topo";
+import AreaBotoes from "./components/AreaBotoes";
 
 export default function Home() {
   return (
     <div>
       <Topo />
+      
+      <AreaBotoes />
+
+      
     </div>
   );
 }
