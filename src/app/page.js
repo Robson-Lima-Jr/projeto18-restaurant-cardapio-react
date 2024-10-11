58ad749fa4baa8d806fec1cd60cd4e1beea3b302
Robson-Jr <robsonjuniorsn@gmail.com>
2024-10-11 15:04:17 -0300

    area de busca criada

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,35 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 // Importação Componentes
 import Topo from "./components/Topo";
 import AreaBotoes from "./components/AreaBotoes";
 
+// Importação imagens
+import Lupa from "/public/lupa.png";
 
 export default function Home() {
   return (
     <div>
       <Topo />
-      
+
       <main className={styles.container_base}>
         <AreaBotoes />
 
+        <div className={styles.container_busca}>
+          <Image src={Lupa} alt="Lupa" className={styles.lupa_img}></Image>
 
+          <input 
+          className={styles.input_busca}
+          type="text" 
+          name="busca" 
+          id="idBuca" 
+          placeholder="Pesquise aqui um dos pratos do nosso cardápio..."
+          />
+        </div>
       </main>
 
-      
+
     </div>
   );
 }
