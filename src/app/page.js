4d35f1d1e2d9593d88a788a90c8fa1bd9b2b11b3
Robson-Jr <robsonjuniorsn@gmail.com>
2024-10-15 10:45:05 -0300

    atualizar cards

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 
+
 // Importação Componentes
 import Topo from "./components/Topo";
 import AreaBotoes from "./components/AreaBotoes";
+import Card from "./components/Card";
 
 // Importação imagens
 import Lupa from "/public/lupa.png";
+
+// Importação de dados
+import { produtos } from "./data/dados.js";
 
 export default function Home() {
   return (
@@ -19,14 +24,32 @@ export default function Home() {
         <div className={styles.container_busca}>
           <Image src={Lupa} alt="Lupa" className={styles.lupa_img}></Image>
 
-          <input 
-          className={styles.input_busca}
-          type="text" 
-          name="busca" 
-          id="idBuca" 
-          placeholder="Pesquise aqui um dos pratos do nosso cardápio..."
+          <input
+            className={styles.input_busca}
+            type="text"
+            name="busca"
+            id="idBuca"
+            placeholder="Pesquise aqui um dos pratos do nosso cardápio..."
           />
         </div>
+
+        <section className={styles.container_cardapio}>
+          <h2>Cardápio</h2>
+
+          <div className={styles.cards_area}>
+            {produtos.map((produtos) => (
+              <Card
+                key={produtos.id}
+                imagem={produtos.imagem}
+                nome={produtos.nome}
+                categoria={produtos.categoria}
+                descricao={produtos.descricao}
+                preco={produtos.preco}
+
+              />
+            ))}
+          </div>
+        </section>
       </main>
 
 
